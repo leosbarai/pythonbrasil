c64ab0a7e80f8acd377d8f2894958d9d029c4dcf
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2021-02-04 11:57:08 -0300

    Exercício 16.

--- decisionstructure/16_equation.py
+++ decisionstructure/16_equation.py
@@ -3,7 +3,28 @@ def equation():
     print("      Equação de 2º Grau! ")
     print("********************************** ")
 
-    # fail
+    try:
+        print('Para a resolução da fórmula ax2 + bx + c, informe os valores de')
+        a = int(input('a: '))
+        b = int(input('b: '))
+        c = int(input('c: '))
+
+        if a == 0:
+            print('Não é equação de segundo grau!')
+            exit()
+
+        delta = (b * b) - (4 * a * c)
+
+        if delta < 0:
+            print('Equação não possui raizes reais!')
+        elif delta == 0:
+            print('A equação possui apenas uma raiz real!')
+        else:
+            print('A equação possui duas raiz reais!')
+            
+
+    except ValueError:
+        print('Valores informados para a equação não estão corretos!')
 
 
 if __name__ == "__main__":
